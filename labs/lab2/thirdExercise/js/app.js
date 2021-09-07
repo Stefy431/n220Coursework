f8b905ea9e591f519c48416749079c5eb4b671d0
Stefy431 <56036671+Stefy431@users.noreply.github.com>
2021-09-07 15:31:23 -0400

    comments

--- labs/lab2/thirdExercise/js/app.js
+++ labs/lab2/thirdExercise/js/app.js
@@ -2,18 +2,18 @@ var diam = 1;
 
 function setup() {
     createCanvas(900, 900);
-    background(66, 135, 245)
+    background(66, 135, 245) //background 
 }
 
 function draw() {
 
     diam += 1;
 
-    if(diam >= 200) {
+    if(diam >= 200) {    //when diam reaches 200, goes back to one; 
         diam = 1; 
     }
 
-    circle(450, 450, diam)
+    circle(450, 450, diam)   //diam is the global variable
 
-    console.log(diam);
+    console.log(diam);  //just checking the number of diam
 }
