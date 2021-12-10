ac87c2c14d7c90c8b14019b70891344ad42481d9
Stefy431 <56036671+Stefy431@users.noreply.github.com>
2021-12-10 15:34:06 -0500

    final project

---
+++ labs/final/js/app.js
@@ -0,0 +1,81 @@
+//start of getting all the inputs from the html 
+var userguess = document.getElementById("colorinput"); 
+var output = document.getElementById("output"); 
+var something = document.getElementById("someThing"); 
+var square = document.getElementById("square"); 
+var results = document.getElementById("results")
+
+//colors available only in lower case
+var colors = ["green", "red", "yellow", "pink", "purple", "blue", "black", "brown", "orange", "grey", "Red", "Blue", "Yellow", "Purple"];
+var coolguesses = []; 
+var numbofwintries = "";  //number of wins that the user has
+var wrongguess = "";  //number of losses that the user has. 
+
+guess = '' //guess made in he function check first color that will then go to another function
+var clearout = '' //function to set off the timer. 
+
+
+function checkFirstColor() {
+    i = Math.floor(Math.random() * colors.length)  //random event
+
+    var compguess = colors[i];  //actual random event 
+    var userguessvs = userguess.value;   //get the value of the input 
+    guess = compguess;   //set this as the value to transfer elsewhere
+
+   // for(x=0; x<colors.length; x++) {
+        if(compguess == userguessvs) {
+            output.innerHTML = "You Win";
+            var numbofwintries = numbofwintries +1; 
+    
+            //results.innerHTML = "Number of wins: " + numbofwintries + "" + "Number of losses: " + wrongguess; 
+            
+    
+    
+            timeThing()
+             
+        }
+        /*else if (userguessvs != colors[x]) {
+            coolguesses.push(userguessvs); 
+            console.log(coolguesses); 
+            output.innerHTML = "cool guesses so far that are not on my color list:" + coolguesses[coolguesses.length]
+        } */
+         else if (userguessvs == null) {
+            output.innerHTML = "please enter a color!"
+        
+        }
+        else {
+            output.innerHTML = "Not Quite! "; 
+            something.innerHTML = "Click the square to reveal the answer."; 
+            wrongguess = wrongguess +1; 
+            userguessvs = ""; 
+            //console.log(results.innerHTML = "Number of wins: " + numbofwintries + "" + "Number of losses: " + wrongguess)
+            console.log(wrongguess); 
+            timeThing()
+        
+        }
+        
+    //}
+    
+}
+
+function revealedColor() {
+    square.style.backgroundColor = guess;
+    console.log(guess)
+    timeThing()
+}
+
+
+
+function timeThing() {
+    clearout = setTimeout(clearGme, 3000);
+  }
+
+function clearGme() {
+   square.style.backgroundColor = "#000"
+   something.innerHTML = "Play again :)"
+
+}
+
+
+
+
